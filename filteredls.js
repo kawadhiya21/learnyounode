00ebf749d3129511044b43e64cc553cc1002f54c
Kshitij Awadhiya <kawadhiya21@gmail.com>
2016-05-31 19:25:18 +0530

    Fix Indentation

--- filteredls.js
+++ filteredls.js
@@ -3,12 +3,12 @@ var folderPath = process.argv[2];
 var ext = process.argv[3];
 
 fs.readdir(folderPath, function (err, files) {
-	var filtered = files.filter(function(file) {
-		var dotSplit = file.split('.');
-		return (ext == dotSplit[dotSplit.length - 1] && dotSplit.length > 1);
-	});
+  var filtered = files.filter(function(file) {
+    var dotSplit = file.split('.');
+    return (ext == dotSplit[dotSplit.length - 1] && dotSplit.length > 1);
+  });
 
-	filtered.forEach(function(file) {
-		console.log(file);
-	});
+  filtered.forEach(function(file) {
+    console.log(file);
+  });
 });
