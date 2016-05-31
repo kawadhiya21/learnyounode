30a05cc0797686df36ffe928a95c9ee9ebb17ae7
Kshitij Awadhiya <kawadhiya21@gmail.com>
2016-05-31 19:25:38 +0530

    Fix Indentation

--- httpcollect.js
+++ httpcollect.js
@@ -2,13 +2,13 @@ var http = require('http');
 var path = process.argv[2];
 
 http.get(path, function (response) {
-	var data = '';
-	response.on('data', function (chunk) {
-		data += chunk.toString();
-	});
+  var data = '';
+  response.on('data', function (chunk) {
+    data += chunk.toString();
+  });
 
-	response.on('end', function () {
-		console.log(data.length);
-		console.log(data);
-	});
+  response.on('end', function () {
+    console.log(data.length);
+    console.log(data);
+  });
 });
