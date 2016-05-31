9e087709fe866591a973f8528f7ad23092fde72a
Kshitij Awadhiya <kawadhiya21@gmail.com>
2016-05-31 19:24:37 +0530

    Fix Indentation

--- httpclient.js
+++ httpclient.js
@@ -2,7 +2,7 @@ var http = require('http');
 var path = process.argv[2];
 
 http.get(path, function (response) {
-	response.on('data', function (chunk) {
-		console.log(chunk.toString());
-	});
+  response.on('data', function (chunk) {
+    console.log(chunk.toString());
+  });
 });
